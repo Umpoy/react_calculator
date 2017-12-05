b79a0a14b02f07ec035b534dd9637bab28afbc82
Umpoy <iumpoy@yahoo.com>
2017-12-04 16:54:10 -0800

    Completed Skeleton

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+import React, { Component } from 'react';
 
-const App = () => (
-    <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
-    </div>
-);
+export default class App extends Component {
 
-export default App;
+    render() {
+        return (
+            <div className="phone">
+
+                <div className="text-center" id="calculator">
+
+                    <div className="output">
+                        <span>0</span>Name
+				</div>
+                    <div className="grey btn" id="clear">AC</div>
+                    <div className="grey btn" id="clearOne">CE</div>
+                    <div className="grey btn" id="b%">%</div>
+                    <div className="operator orange btn" id="b/">÷</div>
+                    <div className="number white btn">7</div>
+                    <div className="number white btn">8</div>
+                    <div className="number white btn">9</div>
+                    <div className="operator orange btn">x</div>
+                    <div className="number white btn">4</div>
+                    <div className="number white btn">5</div>
+                    <div className="number white btn">6</div>
+                    <div className="operator orange btn">-</div>
+                    <div className="number white btn">1</div>
+                    <div className="number white btn">2</div>
+                    <div className="number white btn">3</div>
+                    <div className="operator orange btn">+</div>
+                    <div className="number white btn" id="double">0</div>
+                    <div className="number white btn">.</div>
+                    <div className="orange btn equal">=</div>
+                </div>
+                <div className="circle text-center"></div>
+            </div>
+        );
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,26 @@
-const { resolve } = require('path');
-const webpack = require('webpack');
-const PORT = process.env.PORT || 3000;
-
 module.exports = {
-    entry: [
-        'babel-polyfill',
-        'webpack-dev-server/client?http://localhost:' + PORT,
-        './index.js'
-    ],
-    output: {
-        filename: 'bundle.js',
-        path: resolve(__dirname, 'dist'),
-        publicPath: '/'
-    },
-    context: resolve(__dirname, 'src'),
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.js|.jsx?$/,
-                use: [ 'babel-loader' ],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader'
-            },
-            {
-                test: /\.(gif|png|jpe?g|svg)$/i,
-                use: [ 'file-loader?hash=sha512&digest=hex&name=assets/images/[hash].[ext]',
-                    'image-webpack-loader?bypassOnDebug' ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        outputPath: 'assets/fonts/'
-                    }
-                }
-            }
-        ]
-    },
-    plugins: [
-        new webpack.NamedModulesPlugin()
-    ]
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
 };
